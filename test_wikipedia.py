c248ce6b98f02cd8d59729f18d73b586b4d7345f
darigasekerbek <129574982+darigasekerbek@users.noreply.github.com>
2025-01-21 02:34:03 +0500

    Initial commit

--- test_wikipedia.py
+++ test_wikipedia.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Устанавливаем логирование
 logging.basicConfig(
     filename="wikipedia_test_log.log",
     level=logging.INFO,
@@ -18,44 +17,40 @@ logging.basicConfig(
 def test_wikipedia():
     try:
         logging.info("====== WebDriver manager ======")
-        # Инициализация WebDriver с использованием Service
-        service = Service(ChromeDriverManager().install())  # Установка и использование chromedriver через webdriver-manager
+        
+        service = Service(ChromeDriverManager().install())  
         driver = webdriver.Chrome(service=service)
 
         logging.info("Открытие Wikipedia...")
 
-        # Открытие Wikipedia
+        
         driver.get("https://www.wikipedia.org")
         
-        # Убедимся, что страница загружена, подождав появления элемента
+       
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, "search")))
 
         logging.info("Запрос 'Python (programming language)' отправлен.")
         
-        # Ввод поиска в строку
         search_box = driver.find_element(By.NAME, "search")
         search_box.send_keys("Python (programming language)")
         search_box.send_keys(Keys.RETURN)
 
-        # Ожидание появления результатов поиска
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "mp-upper")))
 
         logging.info("Результаты поиска загружены.")
 
-        # Ожидание появления заголовка статьи
         header = driver.find_element(By.ID, "firstHeading")
         if "Python" in header.text:
             logging.info(f"Заголовок статьи: {header.text}")
         else:
             logging.error(f"Ошибка: Заголовок не соответствует ожиданиям.")
         
-        # Закрытие браузера
         driver.quit()
         logging.info("Браузер закрыт.")
     
     except Exception as e:
         logging.error(f"Ошибка: {e}")
-        driver.quit()  # Закрытие браузера в случае ошибки
+        driver.quit() 
 
 if __name__ == "__main__":
     test_wikipedia()
