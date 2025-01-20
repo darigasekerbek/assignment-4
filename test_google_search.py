670444e01e30a6418f8a817747a15fcb18a21dce
darigasekerbek <129574982+darigasekerbek@users.noreply.github.com>
2025-01-21 02:34:34 +0500

    Initial commit

--- test_google_search.py
+++ test_google_search.py
@@ -8,35 +8,35 @@ import pytest
 import os
 from PIL import Image
 
-# Инициализация Chrome WebDriver
+
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Запуск в фоновом режиме
+chrome_options.add_argument("--headless") 
 chrome_options.add_argument("--disable-gpu")
 
-# Устанавливаем драйвер
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 def test_google_search():
     try:
-        # Открытие страницы Google
+       
         driver.get("https://www.google.com")
 
-        # Проверка, что мы на главной странице Google
+        
         assert "Google" in driver.title
 
-        # Сохранение скриншота на главной странице
+      
         screenshot_path = "screenshot_google_home.png"
         driver.save_screenshot(screenshot_path)
         print(f"Screenshot saved at {screenshot_path}")
 
-        # Поиск поля поиска и ввод запроса
+       
         search_box = driver.find_element(By.NAME, "q")
         search_box.send_keys("Test search")
         search_box.submit()
 
-        time.sleep(3)  # Ждем загрузку результатов
+        time.sleep(3)  
 
-        # Сохранение скриншота после поиска
+      
         screenshot_after_search = "screenshot_google_search.png"
         driver.save_screenshot(screenshot_after_search)
         print(f"Screenshot saved at {screenshot_after_search}")
@@ -46,9 +46,9 @@ def test_google_search():
         raise e
 
     finally:
-        # Закрытие браузера
+        
         driver.quit()
 
-# Запуск теста
+
 if __name__ == "__main__":
     pytest.main()
